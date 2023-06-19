fc3b6e4a56fb444436c9ed5a17c40fe9f59e9de3
Pawel Pesz <pawel.pesz@gmail.com>
2023-06-19 14:59:09 +0100

    feat: Terraform Kubernetes provider configuration * Explicit config is required for aws-auth management to work.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,3 +9,14 @@ provider "aws" {
     })
   }
 }
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+# See: https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1280#issuecomment-804499461
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.4"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.21"
+    }
   }
   cloud {
     # organization from TF_CLOUD_ORGANIZATION
