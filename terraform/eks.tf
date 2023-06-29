a9911efd6a144fbdedbf045fa07cc473cd8a9806
Pawel Pesz <pawel.pesz@gmail.com>
2023-06-29 09:48:50 +0100

    feat: Prometheus EBS persistence * Tweaked the EBS CSI driver role's prefix.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -118,7 +118,7 @@ module "ebs_csi_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.22"
 
-  role_name_prefix      = "ebs-csi"
+  role_name_prefix      = "ebs-csi-"
   attach_ebs_csi_policy = true
 
   oidc_providers = {
