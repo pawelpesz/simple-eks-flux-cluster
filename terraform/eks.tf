5150baf75a55ca873617164e3fdb2a332ce7f19e
Pawel Pesz <pawel.pesz@gmail.com>
2023-07-10 16:19:20 +0100

    fix: IAM role & policy naming * Loki role name should not be random. * Let's prefix all roles and policies with cluster name for consistency.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -118,7 +118,7 @@ module "ebs_csi_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.27"
 
-  role_name_prefix      = "ebs-csi-"
+  role_name_prefix      = "${var.cluster_name}-ebs-csi-"
   attach_ebs_csi_policy = true
 
   oidc_providers = {
