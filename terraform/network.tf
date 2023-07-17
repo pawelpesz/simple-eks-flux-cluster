eb0f27c783f466d55ba16504e78d074287adab75
pawelpesz <81652898+pawelpesz@users.noreply.github.com>
2023-07-17 15:36:08 +0100

    feat: VPC Endpoints & EC2 Instance Connect Endpoints (#49)

--- terraform/network.tf
+++ terraform/network.tf
@@ -11,7 +11,7 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "~> 5.1"
 
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
@@ -35,4 +35,70 @@ module "vpc" {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"           = "1"
   }
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "~> 5.1"
+
+  vpc_id = module.vpc.vpc_id
+
+  create_security_group      = true
+  security_group_name_prefix = "${var.cluster_name}-vpc-endpoints-"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from VPC"
+      cidr_blocks = [module.vpc.vpc_cidr_block]
+    }
+  }
+
+  # See: https://docs.aws.amazon.com/eks/latest/userguide/private-clusters.html
+  endpoints = {
+    s3 = {
+      service = "s3"
+      #tags    = { Name = "${var.cluster_name}-s3" }
+    },
+    ec2 = {
+      service             = "ec2"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    elasticloadbalancing = {
+      service             = "elasticloadbalancing"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    logs = {
+      service             = "logs"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    sts = {
+      service             = "sts"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    kms = {
+      service             = "kms"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+  }
+}
+
+resource "aws_ec2_instance_connect_endpoint" "endpoint" {
+  for_each           = toset(module.vpc.private_subnets)
+  subnet_id          = each.key
+  preserve_client_ip = true
 }
