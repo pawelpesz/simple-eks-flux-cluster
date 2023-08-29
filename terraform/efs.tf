872addb0d3246b2b9a5b22e205c2245ea64bc949
Pawel Pesz <pawel.pesz@gmail.com>
2023-08-29 15:44:45 +0100

    feat: Update components * Terraform AWS provider 5.8 -> 5.14 * Terraform Kubernetes provider 2.22 -> 2.23 * Terraform Flux provider 1.0 -> 1.1 * Terraform EKS module 19.15 -> 19.16 * Terraform IAM module 5.27 -> 5.29 * Flux 2.0.1 -> 2.1.0

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -24,7 +24,7 @@ module "efs" {
 
 module "efs_csi_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.27"
+  version = "~> 5.29"
 
   role_name             = var.efs_csi_irsa_role_name
   attach_efs_csi_policy = true
